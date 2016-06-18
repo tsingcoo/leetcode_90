7be7cad0b565916c6d106d5dd52957a11a75528e
wangql <wangql@nlp.nju.edu.cn>
2016-06-18 19:58:58 +0800

    Initial Commit

---
+++ leetcode_90/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode_90
+//
+//  Created by 王青龙 on 6/18/16.
+//  Copyright © 2016 王青龙. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
